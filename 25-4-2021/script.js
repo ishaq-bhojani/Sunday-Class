09f5c37995e3f41ca62d51db1580ef124676eae2
Ishaq Bhojani <ishaqbhojani@gmail.com>
2021-04-25 12:13:16 +0500

    25-4-2021 class code JS

---
+++ 25-4-2021/script.js
@@ -0,0 +1,40 @@
+// alert('Hello World');
+
+// Data Types
+
+// 1. String
+// "Saylani"
+
+// 2. Number
+// 123
+
+// 3. Boolean
+// true
+
+// 4. undefined
+// undefined
+
+// 5. null
+// null
+
+// 6. Object
+/* {
+  leafColor: "Green",
+  branches: 4
+} */
+
+var abc = 'Hello World!';
+var jkl = ' Saylani';
+var def = 8;
+var ghi = true;
+document.write(jkl + def + def + ghi);
+
+//  8 + 8 + 'Saylani'
+// 16 + 'Saylani' // String
+
+// 'Saylani' + 8 + 8
+// 'Saylani8' + 8
+// 'Saylani88'
+
+
+
