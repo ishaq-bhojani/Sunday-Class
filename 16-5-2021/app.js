c856ae955ef9f52fc6f5b2447f7b45567ff51bab
Ishaq Bhojani <ishaqbhojani@gmail.com>
2021-05-16 14:23:05 +0500

    16-5-2021 class code

---
+++ 16-5-2021/app.js
@@ -0,0 +1,40 @@
+// var num = Number(prompt("Enter a number"));
+// // num = 9
+
+// if (isNaN(num)) {
+//   console.log("Please enter a number");
+// } else if (num % 2 === 0) {
+//   console.log("This number is Even");
+// } else {
+//   console.log("This number is Odd");
+// }
+
+// var num = Number(prompt("Enter a number"));
+// // num = 15; FizzBuzz
+// // num = 9; Fizz
+// // num = 10; Buzz
+// // num = 17; num
+
+// if (num % 3 === 0 && num % 5 === 0){
+//     console.log("FizzBuzz");
+// } else if (num % 3 === 0) {
+//     console.log("Fizz");
+// } else if (num % 5 === 0) {
+//     console.log("Buzz");
+// } else {
+//     console.log(num);
+// }
+
+// All these things will pass the "if" condition
+// true "a" 1 23456789 [] {}
+
+// All these things will fail the "if" condition
+// false 0 "" null undefined
+
+// initialization = var i = 0;
+// condition = i < 10;
+// increment = i++;
+// var num = 5;
+// for (var i = 1; i <= 10; i++) {
+//     console.log(num + " X " + i + " = " + (num*i));
+// }
