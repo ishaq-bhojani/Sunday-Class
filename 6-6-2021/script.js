66e6710b4415a391ae80b401b407448c13005334
Ishaq Bhojani <ishaqbhojani@gmail.com>
2021-06-06 12:00:03 +0500

    6-6-2021 class code

---
+++ 6-6-2021/script.js
@@ -0,0 +1,41 @@
+// var arr = ["Mango", "Apple", "Banana"];
+// var userData = prompt("Write a fruit name.");
+// var isAvailabe = false;
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (userData.toLowerCase() === arr[i].toLowerCase()) {
+//     isAvailabe = true;
+//     break;
+//   }
+// }
+
+// if(isAvailabe) {
+//   console.log("This fruit is available");
+// } else {
+//   console.log("This fruit is not available");
+// }
+
+// var string = "Hello world";
+// console.log(string.slice(6,11));
+
+// var fruit = "Banana";
+// var arr = ["Mango", "Apple", "Banana"];
+// console.log(arr.indexOf(fruit));
+
+// var forbiddenWord = "Saley";
+// var sentence = prompt("Write a sentence");
+// var index = sentence.indexOf(forbiddenWord);
+// if(index !== -1) {
+//   var before = sentence.slice(0, index);
+//   var after = sentence.slice(index + forbiddenWord.length, sentence.length);
+//   console.log(before + after);
+//   console.log("This has forbidden word");
+// } else {
+//   console.log("All clear");
+// }
+
+// Math.round(0.45);
+// Math.ceil(0.1);
+// Math.floor(0.9);
+
+// var date = new Date();
