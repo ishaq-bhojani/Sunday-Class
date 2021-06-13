c8350c81d821dd695fa7f22ae424d9116669c05c
Ishaq Bhojani <ishaqbhojani@gmail.com>
2021-06-13 12:03:38 +0500

    13-6-21 class code

---
+++ 13-6-2021/script.js
@@ -0,0 +1,29 @@
+// var date = new Date();
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// console.log(date.getDay());
+// console.log(dayNames[date.getDay()]);
+// console.log(date.getDate());
+// var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+// console.log(months[date.getMonth()]);
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getTime());
+// var year = date.getFullYear().toString().slice(2);
+// var month = months[date.getMonth()];
+// var todayDate = date.getDate();
+// console.log(todayDate+"-"+month+"-"+year);
+
+// var today = new Date();
+// var doomsday = new Date("June 30, 2035");
+// var msToday = today.getTime();
+// var msDoomsday = doomsday.getTime();
+// var msDiff = msDoomsday - msToday;
+// console.log(msDiff);
+
+// var date = new Date();
+// console.log(date);
+// date.setDate(5);
+// date.setMonth(1);
+// date.setFullYear(1940);
+// console.log(date);
